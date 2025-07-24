40a4f80c1087cb82a005d6a8da60586982d110a5
.newmind <git.newmind@newmind.pw>
2025-07-24 14:02:15 +0200

    permission shim experiment

---
+++ permissionshim/src/Polysemy.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE Trustworthy #-}
+
+-- ghc-pkg field polysemy exposed-modules --simple-output
+
+
+module Polysemy (module Polysemy) where
+
+import "polysemy" Polysemy
