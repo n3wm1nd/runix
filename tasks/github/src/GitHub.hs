f5186a67d6070fba29ce167b23215e5815bd860b
.newmind <git.newmind@newmind.pw>
2025-07-27 05:26:15 +0200

    refactoring and api interface generation [still untested, example only]

--- tasks/github/src/GitHub.hs
+++ tasks/github/src/GitHub.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications #-}
 
 -- | GitHub domain operations and task re-exports
 --
@@ -25,14 +26,23 @@ module GitHub
   ( -- * Types
     Repository,
     RepositoryName,
+    GitHub(..),
     createRepo,
   )
 where
 
 
 import qualified GitHub.API as API
-import GitHub.Effects (GitHubApi)
 import Polysemy
+import Runix.Effects 
+
+newtype GitHub = GitHub {
+    token :: String
+}
+
+instance RestEndpoint GitHub where
+    apiroot _ = "https://api.github.com/"
+    authheaders p = [("Authorization", "Bearer " <> p.token)]
 
 -- Domain operations (GitHub API calls)
 -- These use RestAPI effect to make actual GitHub API calls
@@ -48,8 +58,35 @@ isprivate Public = False
 data Repository = Repository {name :: RepositoryName, owner :: GithubUser}
 
 -- Create a new repository on GitHub 
-createRepo :: (Members '[GitHubApi] r) => 
+createRepo :: Members '[RestAPI GitHub] r => 
   RepositoryName -> RepositoryDescription -> RepositoryVisibility -> Sem r Repository
 createRepo (RepositoryName name) (RepositoryDescription description) visibility = do
-  repo <- API.createRepo name description (isprivate visibility)
-  return  Repository {name = RepositoryName repo.name, owner = GithubUser repo.owner}
+  let req = API.RepositoryCreateRequest 
+              { API.name = name
+              , API.description = Just description
+              , API.homepage = Nothing
+              , API.private = Just (isprivate visibility)
+              }
+  (repo :: API.RepositoryCreateResponse) <- post @GitHub (Endpoint "/user/repos") req
+  return Repository {name = RepositoryName repo.name, owner = GithubUser (API.login $ API.owner repo)}
+
+-- List all repositories for the authenticated user
+-- listRepos :: Members '[RestAPI GitHub] r => Sem r [Repository]
+
+-- Get details of a specific repository
+-- getRepo :: Members '[RestAPI GitHub] r => RepositoryName -> Sem r Repository
+
+-- Delete a repository
+-- deleteRepo :: Members '[RestAPI GitHub] r => RepositoryName -> Sem r ()
+
+-- Create or update a file in a repository
+-- createFile :: Members '[RestAPI GitHub] r => RepositoryName -> FilePath -> String -> Sem r ()
+
+-- Read content of a file from a repository
+-- readFile :: Members '[RestAPI GitHub] r => RepositoryName -> FilePath -> Sem r String
+
+-- Create a new issue
+-- createIssue :: Members '[RestAPI GitHub] r => RepositoryName -> String -> String -> Sem r ()
+
+-- List issues for a repository
+-- listIssues :: Members '[RestAPI GitHub] r => RepositoryName -> Sem r [Issue]
