240f3c5ecfba65497c75800b45fd60b0252887c5
.newmind <git.newmind@newmind.pw>
2026-01-07 20:01:39 +0100

    add mkdir and rm to filesystem effect

--- src/Runix/FileSystem/Effects.hs
+++ src/Runix/FileSystem/Effects.hs
@@ -30,7 +30,7 @@ import Control.Monad (forM)
 import qualified System.Directory
 import qualified System.FilePath.Glob as Glob
 import Runix.Logging.Effects (Logging, info)
-import System.IO.Error (tryIOError)
+import System.IO.Error (tryIOError, userError)
 
 -- | Read-only filesystem operations
 data FileSystemRead (m :: Type -> Type) a where
@@ -71,10 +71,20 @@ makeAbsolute path
 -- | Write-only filesystem operations
 data FileSystemWrite (m :: Type -> Type) a where
     WriteFile :: FilePath -> ByteString -> FileSystemWrite m (Either String ())
+    CreateDirectory :: Bool -> FilePath -> FileSystemWrite m (Either String ())  -- Bool for createParents
+    Remove :: Bool -> FilePath -> FileSystemWrite m (Either String ())  -- Bool for recursive
 
 -- | Public API function that converts Either to Fail
 writeFile :: Members [FileSystemWrite, Fail] r => FilePath -> ByteString -> Sem r ()
 writeFile p d = send (WriteFile p d) >>= either fail return
+
+-- | Create a directory, optionally creating parent directories
+createDirectory :: Members [FileSystemWrite, Fail] r => Bool -> FilePath -> Sem r ()
+createDirectory createParents p = send (CreateDirectory createParents p) >>= either fail return
+
+-- | Remove a file or directory, optionally recursive for directories
+remove :: Members [FileSystemWrite, Fail] r => Bool -> FilePath -> Sem r ()
+remove recursive p = send (Remove recursive p) >>= either fail return
 
 -- | Combined filesystem effect for backwards compatibility
 -- This is a type alias that combines both read and write operations
@@ -120,8 +130,12 @@ limitedAccessWrite :: (forall m x. FileSystemWrite m x -> AccessPermission) -> S
 limitedAccessWrite isAllowed = intercept $ \action -> case isAllowed action of
     AllowAccess -> case action of
         WriteFile f c -> send (WriteFile f c)
+        CreateDirectory p f -> send (CreateDirectory p f)
+        Remove r f -> send (Remove r f)
     ForbidAccess reason -> case action of
         WriteFile _ _ -> return (Left ("not allowed: " ++ reason))
+        CreateDirectory _ _ -> return (Left ("not allowed: " ++ reason))
+        Remove _ _ -> return (Left ("not allowed: " ++ reason))
 
 -- | Limit read operations to a subpath
 -- Relative paths are resolved using the actual CWD, then checked against allowedPath
@@ -194,6 +208,14 @@ limitSubpathWrite allowedPath action = do
       case checkPath cwd targetPath of
         AllowAccess -> send (WriteFile targetPath content)
         ForbidAccess reason -> return (Left ("not allowed: " ++ reason))
+    CreateDirectory createParents targetPath ->
+      case checkPath cwd targetPath of
+        AllowAccess -> send (CreateDirectory createParents targetPath)
+        ForbidAccess reason -> return (Left ("not allowed: " ++ reason))
+    Remove recursive targetPath ->
+      case checkPath cwd targetPath of
+        AllowAccess -> send (Remove recursive targetPath)
+        ForbidAccess reason -> return (Left ("not allowed: " ++ reason))
     ) action
   where
     checkPath :: FilePath -> FilePath -> AccessPermission
@@ -222,6 +244,8 @@ chrootSubpathRead chrootPath = intercept $ \case
 chrootSubpathWrite :: FilePath -> Sem (FileSystemWrite : r) a -> Sem (FileSystemWrite : r) a
 chrootSubpathWrite chrootPath = intercept $ \case
     WriteFile f c -> send (WriteFile (chrootPath </> f) c)
+    CreateDirectory p f -> send (CreateDirectory p (chrootPath </> f))
+    Remove r f -> send (Remove r (chrootPath </> f))
 
 -- | Hide dotfiles for read operations
 hideDotfilesRead :: Sem (FileSystemRead : r) a -> Sem (FileSystemRead : r) a
@@ -246,6 +270,10 @@ hideDotfilesWrite :: Sem (FileSystemWrite : r) a -> Sem (FileSystemWrite : r) a
 hideDotfilesWrite = intercept $ \case
     WriteFile f c | isDotfile f -> return (Left ("Access to dotfile denied: " ++ f))
                   | otherwise -> send (WriteFile f c)
+    CreateDirectory p f | isDotfile f -> return (Left ("Access to dotfile denied: " ++ f))
+                        | otherwise -> send (CreateDirectory p f)
+    Remove r f | isDotfile f -> return (Left ("Access to dotfile denied: " ++ f))
+               | otherwise -> send (Remove r f)
   where
     isDotfile :: FilePath -> Bool
     isDotfile path = case takeFileName path of
@@ -285,6 +313,25 @@ filesystemWriteIO = interpret $ \case
     WriteFile p d -> do
         info $ fromString "writing file: " <> fromString p
         embed (tryIOError $ BS.writeFile p d) >>= return . either (Left . show) Right
+    CreateDirectory createParents p -> do
+        info $ fromString "creating directory: " <> fromString p <> (if createParents then fromString " (with parents)" else fromString "")
+        embed (tryIOError $ System.Directory.createDirectoryIfMissing createParents p) >>= return . either (Left . show) Right
+    Remove recursive p -> do
+        info $ fromString "removing: " <> fromString p <> (if recursive then fromString " (recursive)" else fromString "")
+        embed (tryIOError $ removePathSafe recursive p) >>= return . either (Left . show) Right
+  where
+    -- Safe removal that handles both files and directories
+    removePathSafe :: Bool -> FilePath -> IO ()
+    removePathSafe recursive path = do
+        isDir <- System.Directory.doesDirectoryExist path
+        isFile <- System.Directory.doesFileExist path
+        if isDir
+            then if recursive
+                 then System.Directory.removeDirectoryRecursive path
+                 else System.Directory.removeDirectory path
+            else if isFile
+                 then System.Directory.removeFile path
+                 else ioError $ userError $ "Path does not exist: " ++ path
 
 -- | Combined IO interpreter for backwards compatibility
 -- Interprets both FileSystemRead and FileSystemWrite
